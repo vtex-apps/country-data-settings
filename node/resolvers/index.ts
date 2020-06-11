4a610ed92dba603090930342d227ab6dd67d466c
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-06-11 10:05:43 -0300

    add locationSelect field to graphql types

--- node/resolvers/index.ts
+++ node/resolvers/index.ts
@@ -1,6 +1,10 @@
+import { GraphQLJSON, GraphQLJSONObject } from 'graphql-type-json'
+
 import { queries as countryQueries } from './country'
 
 export default {
+  JSON: GraphQLJSON,
+  JSONObject: GraphQLJSONObject,
   Query: {
     ...countryQueries,
   },
