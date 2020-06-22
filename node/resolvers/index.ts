a7140eb297b2ce140337a4e2d161e79c135f9b3a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-06-22 15:02:48 -0300

    use interface for common fields in FieldData and PostalCodeData

--- node/resolvers/index.ts
+++ node/resolvers/index.ts
@@ -5,6 +5,11 @@ import { queries as countryQueries } from './country'
 export default {
   JSON: GraphQLJSON,
   JSONObject: GraphQLJSONObject,
+  Field: {
+    __resolveType(root: any) {
+      return root.label === 'postalCode' ? 'PostalCodeData' : 'FieldData'
+    },
+  },
   Query: {
     ...countryQueries,
   },
